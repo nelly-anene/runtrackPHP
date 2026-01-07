396893ca00c3325429f028763355691b60edcdc0
nelly.anene <nelly.anene@laplateforme.io>
2026-01-07 23:00:34 +0100

    List the Prime numbers up to 1000, by  putting a line break between each number

--- jour02/job05/index.php
+++ jour02/job05/index.php
@@ -1,6 +1,22 @@
 <?php   
-$i = 0;
-for ($i=0; $i <= 1000 ; $i++) { 
-echo("$i <br/>"); 
+//boucle de 2 à 1000
+for ($nombre = 2; $nombre <= 1000; $nombre++) {
+    //comme le nombre est premier
+    $estPremier = true;
+
+    //verification de diviseurs de 2 jusqu" à rc
+    for ($diviseurs = 2; $diviseurs <= sqrt($nombre); $diviseurs++) {
+    //si le reste est 0, donc pas Premier alors
+    if ($nombre % $diviseurs == 0) {
+        $estPremier = false; 
+        break; //pas besoin de continuer , on arrête
+    }
 }
+
+//si ecore Premier on l'affiche
+    if ($estPremier) {
+        echo $nombre . "<br>"; //pour une nouvelle ligne
+    }
+}
+
 ?>
