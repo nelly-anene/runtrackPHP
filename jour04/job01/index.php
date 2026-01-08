553c9e2e726197a930b87d982253c0357ebe72d9
nelly.anene <nelly.anene@laplateforme.io>
2026-01-08 16:34:31 +0100

    Jour04 7 jobs dossiers

---
+++ jour04/job01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Job01 - Argumentzz GET</title>
+</head>
+<body>
+    <h1>Nombre d'arguments GET : <?php echo count($_GET); ?></h1>
+
+    <?php if (count($_GET) > 0): ?>
+        <h3>Arguments reçus :</h3>
+        <ul>
+        <?php foreach ($_GET as $key => $value): ?>
+
+    </body>
+    </html>
