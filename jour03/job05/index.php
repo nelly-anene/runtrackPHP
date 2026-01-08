6ffff00ca36adf62d2ba874fc0a414959eae08f2
nelly.anene <nelly.anene@laplateforme.io>
2026-01-08 16:29:39 +0100

    Jour04 structure complète

---
+++ jour03/job05/index.php
@@ -0,0 +1,11 @@
+<?php
+// Job 05 - Compter les voyelles et consonnes et afficher dans un tableau HTML
+
+// Création de la string
+$str = "On n est pas le meilleur quand on le croit mais quand on le sait";
+
+// Création du dictionnaire
+$dic = [
+    "voyelles" => 0,
+    "consonnes" => 0
+];
