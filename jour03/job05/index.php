b76eeb93d1aca9701f6aed9cb8f4d05a5bee9d37
nelly.anene <nelly.anene@laplateforme.io>
2026-01-08 22:48:18 +0100

    Results of Vowels and Consonants in display

--- jour03/job05/index.php
+++ jour03/job05/index.php
@@ -9,3 +9,34 @@ $dic = [
     "voyelles" => 0,
     "consonnes" => 0
 ];
+
+$voyelles = "aeiouyAEIOUY";
+
+for ($i = 0; $i < strlen($str); $i++) {
+    $lettre = $str[$i];
+
+    if (ctype_alpha($lettre)) {
+        if (strpos($voyelles, $lettre) !== false) {
+            $dic["voyelles"]++;
+        } else {
+            $dic["consonnes"]++;
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Job 05</title>
+</head>
+<body>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Voyelles</th>
+                <th>Consonnes</th>
+            </tr>
+        </thead>
+        <tbody>
