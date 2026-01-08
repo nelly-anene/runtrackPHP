720fee8e3e55c410e43b8181bd6356d1ee7c4843
nelly.anene <nelly.anene@laplateforme.io>
2026-01-08 01:49:49 +0100

    job01 - Verify is numbers are Even and Odd numbers

---
+++ jour03/job01/index.php
@@ -0,0 +1,11 @@
+<?php
+//verifying if the numbers are Even or Odd numbers
+$nombres = [200, 204, 173, 98, 171, 404, 459];
+foreach ($nombres as $nombre) {
+    if ($nombre % 2 == 0) {
+        echo "$nombre est paire<br />";
+    } else {
+            echo "$nombre est impaire<br />";
+    }
+}   
+?>
