e5d2c4e1582de7b1b173ff5b73393c75afca1aff
nelly.anene <nelly.anene@laplateforme.io>
2026-01-09 15:43:55 +0100

    Function GetHello returned Hello to La Plateforme with a displayed return value

---
+++ jour05/job03/index.php
@@ -0,0 +1,12 @@
+<?php
+//Création de function GetHello()
+
+function GetHello() {
+    Return "Hello La Plateforme";
+}
+
+//Recupération de la valeur de retour
+$bright = GetHello();
+echo $bright;
+
+?>
