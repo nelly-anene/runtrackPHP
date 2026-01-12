08c9d6c03113d40788bf64a602aca6f6bb2daee2
nelly.anene <nelly.anene@laplateforme.io>
2026-01-12 11:21:19 +0100

    jour06 Revision

---
+++ jour06/job01/index.php
@@ -0,0 +1,23 @@
+<?php
+$str = "LaPlateforme";
+echo $str;
+echo "<br><br>";
+
+$str2 = "Vive";
+$str3 = "!";
+echo $str2 . " " . $str . " " . $str3;
+echo "<br><br>";
+
+$val = 6;
+echo "Val = " . $val;
+echo "<br>";
+$val = $val + 4;
+echo "Val après +4 = " . $val;
+echo "<br><br>";
+
+$myBool = true;
+echo "myBool = " . ($myBool ? "true" : "false");
+echo "<br>";
+$myBool = false;
+echo "myBool = " . ($myBool ? "true" : "false");
+?>
